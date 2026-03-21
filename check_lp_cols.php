3ace169bb01402d241c1fe984be3b6cf64a0da03
ishimweghislain <ishimweghislain82@gmail.com>
2026-03-21 16:24:55 +0200

    Removed unwanted files .

--- check_lp_cols.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'config/database.php';
-$c=getConnection();
-$r=$c->query('DESCRIBE loan_instalments');
-while($row=$r->fetch_assoc()) {
-    echo $row['Field'] . "\n";
-}
--- test_cust.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-require_once __DIR__ . '/app.cbfinance.rw/config/database.php';
-$conn = getConnection();
-$res = $conn->query("SELECT customer_code, customer_name, customer_id FROM customers WHERE customer_code = 'C0060' OR customer_code = 'C100000'");
-while($r = $res->fetch_assoc()) print_r($r);
-$lres = $conn->query("SELECT loan_number FROM loan_portfolio WHERE customer_id IN (SELECT customer_id FROM customers WHERE customer_code IN ('C0060', 'C100000'))");
-while($l = $lres->fetch_assoc()) print_r($l);
